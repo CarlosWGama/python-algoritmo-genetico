f4c63618e29b4b66381d1ff21432992aa52fde79
CarlosWGama <carloswgama@gmail.com>
2023-08-23 14:46:32 -0300

    Zerado valores

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
 from ag import AlgoritmoGenetico
 
-dados = [
-    {'item': 'Lapis', 'peso': 1, 'valor': 2},
-    {'item': 'Borracha', 'peso': 1, 'valor': 1},
-    {'item': 'Caderno', 'peso': 3, 'valor': 4},
-    {'item': 'Livro', 'peso': 4, 'valor': 5},
-    {'item': 'Caneta', 'peso': 1, 'valor': 2},
-    {'item': 'Cálculadora', 'peso': 3, 'valor': 3},
+dados = [ 
+
 ]
 
 def funFitness(genes, dados):
-    peso = 0
-    valor = 0
-    
-    for i in range(len(genes)):
-        #Adiciona os itens na bolsa
-        if (genes[i]==1):
-            peso += dados[i]['peso']
-            valor += dados[i]['valor']
-    
-    if (peso > 10):
-        return 0
-    return valor
+    return 0
 
 ag = AlgoritmoGenetico(dados,funcaoFitness=funFitness)
 ag.executa()
